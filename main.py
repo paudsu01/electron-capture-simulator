1ba99fa435bcbddd02999346bbfa1b4cdcdbd982
Supreme Paudel <supremepaudel49@gmail.com>
2024-06-12 15:39:27 -0500

    Use isort and yapf to format the python files

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import re
 import numpy
 import vpython
 
-import simulation_model
 import config
+import simulation_model
 import utils
 from exceptions import (DATFileNotProvidedException,
                         UnableToConvertDATFileToArray)
@@ -19,12 +19,16 @@ def setup_user_input():
     global pause_button
     global speed_slider
     # Run/Pause button
-    pause_button = vpython.button(bind=utils.run_pause_program, text='Pause', background=vpython.color.red)
+    pause_button = vpython.button(bind=utils.run_pause_program,
+                                  text='Pause',
+                                  background=vpython.color.red)
     vpython.scene.append_to_caption("\t\t")
 
     # Camera focus options
     vpython.scene.append_to_caption("Focus camera on: ")
-    vpython.menu(bind=utils.change_camera_focus, choices=['Nucleus', 'Projectile', 'Electron'], index=0)
+    vpython.menu(bind=utils.change_camera_focus,
+                 choices=['Nucleus', 'Projectile', 'Electron'],
+                 index=0)
     vpython.scene.append_to_caption("\t\t")
 
     # Pan mode enable button
@@ -32,19 +36,27 @@ def setup_user_input():
     vpython.scene.append_to_caption("\t\t")
 
     # Screenshot button
-    vpython.button(text='Screenshot', bind=utils.screenshot, background=vpython.color.blue)
+    vpython.button(text='Screenshot',
+                   bind=utils.screenshot,
+                   background=vpython.color.blue)
     vpython.scene.append_to_caption("\n")
 
     # Speed slider
     vpython.scene.append_to_caption("Change the simulation speed:")
-    speed_slider = vpython.slider(bind=utils.change_simulation_rate, value=config.SIM_RATE, min=1, max=150)
+    speed_slider = vpython.slider(bind=utils.change_simulation_rate,
+                                  value=config.SIM_RATE,
+                                  min=1,
+                                  max=150)
     utils.setup_w_text_speed(speed_slider.value)
     vpython.scene.append_to_caption("\n")
 
     # Help and Tips message
-    vpython.scene.append_to_caption('\n\n\n\tHELP/TIPS:\n\nResize canvas by placing mouse at the egde of the canvas\n\n\tTouchpad:\nZoom in/out : Place two fingers on touchpad and move up/down\nRotate "camera" to view scene : Place two fingers on touchpad and press and move \nPan camera : Shift + press touchpad and move (Simulation needs to be paused !!)')
-    vpython.scene.append_to_caption('\n\n\tMouse:\nRight button drag or Ctrl-drag to rotate "camera" to view scene.\nTo zoom, drag with middle button or Alt/Option depressed, or use scroll wheel.\nOn a two-button mouse, middle is left + right.\nShift-drag to pan left/right and up/down.\nTouch screen: pinch/extend to zoom, swipe or two-finger rotate\n')
-
+    vpython.scene.append_to_caption(
+        '\n\n\n\tHELP/TIPS:\n\nResize canvas by placing mouse at the egde of the canvas\n\n\tTouchpad:\nZoom in/out : Place two fingers on touchpad and move up/down\nRotate "camera" to view scene : Place two fingers on touchpad and press and move \nPan camera : Shift + press touchpad and move (Simulation needs to be paused !!)'
+    )
+    vpython.scene.append_to_caption(
+        '\n\n\tMouse:\nRight button drag or Ctrl-drag to rotate "camera" to view scene.\nTo zoom, drag with middle button or Alt/Option depressed, or use scroll wheel.\nOn a two-button mouse, middle is left + right.\nShift-drag to pan left/right and up/down.\nTouch screen: pinch/extend to zoom, swipe or two-finger rotate\n'
+    )
 
 
 def end_simulation():
@@ -56,7 +68,6 @@ def end_simulation():
         vpython.rate(10)
         pass
 
-     
 
 def start_simulation():
 
@@ -70,13 +81,20 @@ def start_simulation():
         vpython.rate(config.SIM_RATE)
         if not config.PAUSED:
 
-            config.PROJECTILE.pos = vpython.vector(SIM.projectile.x, SIM.projectile.y, SIM.projectile.z)
-            config.ELECTRON.pos = vpython.vector(SIM.electron.x, SIM.electron.y, SIM.electron.z)
-            config.NUCLEUS.pos = vpython.vector(SIM.target_nucleus.x, SIM.target_nucleus.y, SIM.target_nucleus.z)
+            config.PROJECTILE.pos = vpython.vector(SIM.projectile.x,
+                                                   SIM.projectile.y,
+                                                   SIM.projectile.z)
+            config.ELECTRON.pos = vpython.vector(SIM.electron.x,
+                                                 SIM.electron.y,
+                                                 SIM.electron.z)
+            config.NUCLEUS.pos = vpython.vector(SIM.target_nucleus.x,
+                                                SIM.target_nucleus.y,
+                                                SIM.target_nucleus.z)
 
             SIM.time += 1
 
     end_simulation()
+
 
 if __name__ == '__main__':
 
@@ -92,7 +110,6 @@ if __name__ == '__main__':
         try:
             data: numpy.ndarray[numpy.ndarray] = numpy.loadtxt(
                 FILENAME, dtype=float, usecols=(i for i in range(0, 10)))
-
 
             ### Init ###
 
--- utils.py
+++ utils.py
@@ -1,15 +1,22 @@
 from __future__ import annotations
 
-import config
-import vpython
-import string
 import random
+import string
+
+import vpython
+
+import config
 
 speed_wtext = None
 camera_pan_button = None
 
-name_to_object_dict = {'Nucleus': config.NUCLEUS, 'Projectile': config.PROJECTILE, 'Electron': config.ELECTRON}
+name_to_object_dict = {
+    'Nucleus': config.NUCLEUS,
+    'Projectile': config.PROJECTILE,
+    'Electron': config.ELECTRON
+}
 latest_object_followed = config.NUCLEUS
+
 
 def enable_pan_mode(event: vpython.vpython.checkbox) -> None:
 
@@ -20,6 +27,7 @@ def enable_pan_mode(event: vpython.vpython.checkbox) -> None:
         vpython.scene.camera.follow(latest_object_followed)
         camera_pan_button.text = "Pan mode: disabled"
 
+
 def change_camera_focus(event: vpython.vpython.menu) -> None:
 
     global latest_object_followed
@@ -28,7 +36,8 @@ def change_camera_focus(event: vpython.vpython.menu) -> None:
     vpython.scene.camera.follow(latest_object_followed)
     camera_pan_button.text = "Enable Pan mode"
 
-def run_pause_program(event : vpython.vpython.button) -> None:
+
+def run_pause_program(event: vpython.vpython.button) -> None:
 
     config.PAUSED = False if config.PAUSED else True
     event.text = 'Run' if event.text == 'Pause' else 'Pause'
@@ -42,12 +51,13 @@ def run_pause_program(event : vpython.vpython.button) -> None:
         vpython.scene.camera.follow(latest_object_followed)
 
 
-def change_simulation_rate(event :vpython.vpython.slider) -> None:
+def change_simulation_rate(event: vpython.vpython.slider) -> None:
 
     config.SIM_RATE = event.value
     setup_w_text_speed(event.value, False)
 
-def setup_w_text_speed(value : int, firstTime : bool = True) -> None:
+
+def setup_w_text_speed(value: int, firstTime: bool = True) -> None:
 
     global speed_wtext
 
@@ -56,18 +66,24 @@ def setup_w_text_speed(value : int, firstTime : bool = True) -> None:
     else:
         speed_wtext.text = value
 
+
 def setup_camera_pan_button() -> None:
 
-    global camera_pan_button 
+    global camera_pan_button
 
-    camera_pan_button = vpython.button(text='Enable Pan mode', bind=enable_pan_mode, background=vpython.color.cyan, disabled=True)
+    camera_pan_button = vpython.button(text='Enable Pan mode',
+                                       bind=enable_pan_mode,
+                                       background=vpython.color.cyan,
+                                       disabled=True)
+
 
 def screenshot(evt: vpython.vpython.button) -> None:
 
     vpython.scene.capture(random_text_generator())
 
+
 def random_text_generator() -> str:
 
-    length = random.randint(8,15)
+    length = random.randint(8, 15)
     options = string.ascii_letters + ''.join([str(i) for i in range(10)])
     return ''.join([random.choice(options) for i in range(length)])
